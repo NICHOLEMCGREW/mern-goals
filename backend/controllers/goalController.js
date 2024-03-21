c470cb2c14c7e460ae1ba77483b19ccc4ec2d6ed
Nichole McGrew <nicholemcgrew@gmail.com>
2024-03-21 09:43:45 -0500

    set up goal route and controller

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,25 @@
+// @desc Get goals
+// @route GET /api/goals
+// @acccess Private
+const getGoals = (req, res) => {
+    res.status(200).json({ message: 'Get goals' })
+}
+
+// @desc Get goals
+// @route GET /api/goals
+// @acccess Private
+const getGoals = (req, res) => {
+    res.status(200).json({ message: 'Get goals' })
+}
+
+// @desc Get goals
+// @route GET /api/goals
+// @acccess Private
+const getGoals = (req, res) => {
+    res.status(200).json({ message: 'Get goals' })
+}
+
+
+module.exports = {
+    getGoals()
+}
