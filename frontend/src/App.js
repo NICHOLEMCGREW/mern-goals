9afe063acfb74d8f3b81fedfc8aa5acac7139914
Nichole McGrew <nicholemcgrew@gmail.com>
2024-03-22 04:46:20 -0500

    cretated dashboard and login pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Dashboard from './pages/Dashboard';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div>
-      <h1>my app</h1>
+    <>
+    <Router>
+    <div className='container'>
+      <Routes>
+        <Route path='/' element={Dashboard}></Route>
+        <Route path='/login' element={Login}></Route>
+        <Route path='/register' element={Register}></Route>
+      </Routes>
     </div>
+    </Router>
+    </>
   );
 }
 
