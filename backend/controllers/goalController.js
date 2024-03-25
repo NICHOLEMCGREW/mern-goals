a358095381d466f90fe39eb8c9712721a8d75548
Nichole McGrew <nicholemcgrew@gmail.com>
2024-03-25 14:08:58 -0500

    completed goal project

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -2,53 +2,51 @@ const asyncHandler = require('express-async-handler')
 
 const Goal = require('../models/goalModel')
 const User = require('../models/userModel')
-// @desc Get goals
-// @route GET /api/goals
-// @acccess Private
 
+// @desc    Get goals
+// @route   GET /api/goals
+// @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find({ user: req.user.id })
+  const goals = await Goal.find({ user: req.user.id })
 
-    res.status(200).json(goals)
+  res.status(200).json(goals)
 })
 
-// @desc Set goal
-// @route POST /api/goals
-// @acccess Private
-
+// @desc    Set goal
+// @route   POST /api/goals
+// @access  Private
 const setGoal = asyncHandler(async (req, res) => {
-    if(!req.body.text) {
-        res.status(400)
-        throw new Error('Please add a text feild')
-    }
-    const goal = await Goal.create({
-        text: req.body.text,
-        user: req.user.id,
-    })
-    res.status(200).json(goal)
+  if (!req.body.text) {
+    res.status(400)
+    throw new Error('Please add a text field')
+  }
+
+  const goal = await Goal.create({
+    text: req.body.text,
+    user: req.user.id,
+  })
+
+  res.status(200).json(goal)
 })
 
-// @desc Update goal
-// @route PUT /api/goals/:id
-// @acccess Private
-
+// @desc    Update goal
+// @route   PUT /api/goals/:id
+// @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
-    const goal = await Goal.findById(req.params.id)
+  const goal = await Goal.findById(req.params.id)
 
-    if(!goal) {
-        res.status(400)
-        throw new Error('Goal not found')
-    }
+  if (!goal) {
+    res.status(400)
+    throw new Error('Goal not found')
+  }
 
-    const user = await User.findById(req.user.id)
+  // Check for user
+  if (!req.user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
 
-    // Check for user
-    if(!user) {
-        res.status(401)
-        throw new Error('User not found')
-    }
-
-     // Make sure the logged in user matches the goal user
+  // Make sure the logged in user matches the goal user
   if (goal.user.toString() !== req.user.id) {
     res.status(401)
     throw new Error('User not authorized')
@@ -65,20 +63,20 @@ const updateGoal = asyncHandler(async (req, res) => {
 // @route   DELETE /api/goals/:id
 // @access  Private
 const deleteGoal = asyncHandler(async (req, res) => {
-    const goal = await Goal.findById(req.params.id)
-  
-    if (!goal) {
-      res.status(400)
-      throw new Error('Goal not found')
-    }
-  
-    // Check for user
+  const goal = await Goal.findById(req.params.id)
+
+  if (!goal) {
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+
+  // Check for user
   if (!req.user) {
     res.status(401)
     throw new Error('User not found')
   }
 
-     // Make sure the logged in user matches the goal user
+  // Make sure the logged in user matches the goal user
   if (goal.user.toString() !== req.user.id) {
     res.status(401)
     throw new Error('User not authorized')
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+const path = require('path')
+const exp = require('constants')
 
 connectDB()
 
@@ -14,6 +16,13 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
+
+// Serve frontend
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+}
 
 app.use(errorHandler)
 
